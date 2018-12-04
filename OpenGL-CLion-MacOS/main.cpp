d0c5e2ea14fd39815dcc9b64b26ea3d14358bd8d
Yu Cao <yu_cao@zju.edu.cn>
2018-12-05 00:08:14 +0800

    Finish of Tutorial "Hello Triangle" and polish the Commit In this tutorial, we learn VAO, VBO, EBO and how to draw the basic shape in OpenGL. I think the most important thing is that we should know how the VAO to control and optimize the VBO and EBO, and use the VAO correctly. We should handle the basic API and basic rendering process by this tutorial.

--- OpenGL-CLion-MacOS/main.cpp
+++ OpenGL-CLion-MacOS/main.cpp
@@ -108,23 +108,30 @@ int main()
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
-    // 1. Set the vertices data
+    // Set the vertices data
     float vertices[] = {
-            -0.5f, -0.5f, 0.0f, // left
-            0.5f, -0.5f, 0.0f,  // right
-            0.0f, 0.5f, 0.0f    // top
+            0.5f, 0.5f, 0.0f,  // top right
+            0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  // bottom left
+            -0.5f, 0.5f, 0.0f   // top left
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+            0, 1, 3,  // first Triangle
+            1, 2, 3   // second Triangle
     };
 
     // Vertex Array Object(VAO) is like array of pointer point to the VBO's different attributes
     // the relation can see the "VAO and VBO relation.png"
     // if you want draw more than one object, you need generate and config all VAO(and necessary VBO and attribute pointer)
     // then when we draw, use VAO, binding it; after draw, unbinding it.
-    unsigned int VBO, VAO;
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
 
     // first para is the number of buffer object name to be generated
     // second para specifies an array in which the generated buffer object names are stored
     glGenBuffers(1, &VBO);// use the Vertex Buffer Object(VBO) to manager the memory which store the vertices in GPU
+
+    glGenBuffers(1, &EBO);// like VBO
 
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
     //---------------------
@@ -136,13 +143,19 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
     // store the vertices data into the memory of GPU
-    //First para: the type of target buffer
-    //Second para: the size of data transport, the better is to use "sizeof"
-    //Third para: the real data we want to send
-    //Fourth para: how to manager these data in GPU
-    //GL_STATIC_DRAW: Data won't or hardly be changed; DYNAMIC:change a lot; STREAM: change in each draw
+    // First para: the type of target buffer
+    // Second para: the size of data transport, the better is to use "sizeof"
+    // Third para: the real data we want to send
+    // Fourth para: how to manager these data in GPU
+    // GL_STATIC_DRAW: Data won't or hardly be changed; DYNAMIC:change a lot; STREAM: change in each draw
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
                  GL_STATIC_DRAW);// we use static because the triangle vertices won't change in each render
+
+    // EBO: reduce the draw of repeat vertices by ordering the order of how to draw
+    // TO learn more, can see the reference "VAO and EBO relation.png"
+    // This part is like VBO(bind and tell the data struct in buffer), just change the type of buffer
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Each vertices property get the data from the VBO which is being binding the GL_ARRAY_BUFFER(if more than one VBO)
     // first para: location of vertices property [layout(location = 0) in vertices shader]
@@ -162,6 +175,9 @@ int main()
     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
     glBindVertexArray(0);
 
+    // uncomment this call to draw in wireframe polygons.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);//set back to the default mode
 
     // render loop
     // -----------
@@ -185,7 +201,14 @@ int main()
         // first para: the type of we want to draw;
         // second para: beginning index of vertices array;
         // third para: how many vertices we want to draw
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // first para: same as glDrawArrays()
+        // second para: the number of vertices we need to draw
+        // third para: the type of index
+        // fourth para: the offset of EBO or a array of index when you don't use the EBO
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,
+                       0);// it will get the index from EBO binding GL_ELEMENT_ARRAY_BUFFER now.
         // glBindVertexArray(0); // no need to unbind it every time
         // ---------------------
 
@@ -199,6 +222,7 @@ int main()
     // ------------------------------------------------------------------------
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
