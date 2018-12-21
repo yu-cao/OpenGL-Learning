2b8541333b81cde10813e97778e4cfc1e5226e13
CaoYu <yu_cao@zju.edu.cn>
2018-12-21 17:49:53 +0800

    Finish the whole part of texture and exercise. Prepare to store to the repository.

--- OpenGL-CLion-MacOS/main.cpp
+++ OpenGL-CLion-MacOS/main.cpp
@@ -13,6 +13,10 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+//Exercise 4:
+float mixValue = 0.2f;
+
+
 int main()
 {
     // glfw: initialize and configure
@@ -46,52 +50,6 @@ int main()
         return -1;
     }
 
-    //-----------------------Texture--------------------------------
-    // refer by ID
-    // glGenTextures:
-    // first is input generate the texture number
-    // second is the array to store the texture ID(now is the single one because is just 1 input)
-    unsigned int texture;
-    glGenTextures(1, &texture);
-
-    // Bind the texture
-    glBindTexture(GL_TEXTURE_2D, texture);
-
-    // set the texture wrapping/filtering options (on the currently bound texture object)
-    // first: target of nD textures
-    // second: options we want to set and for which texture axis
-    // third: texture wrapping mode
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-    // 为层级过滤提供放大/缩小的选项，使用 层级or非层级 线性过滤/邻近过滤？
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);//放大用层级过滤不会产生效果，层级过滤只对缩小有效
-
-    //load the picture as texture
-    int width, height, nrChannels;
-    unsigned char *data = stbi_load("texture/container.jpg", &width, &height, &nrChannels, 0);
-    if (data)
-    {
-        // first: texture target:generate a texture on the currently bound texture object at the same target
-        // second: mipmap level for which we want to create a texture if you want to set level manually, the base level is 0（层级渐远）
-        // third: what kind of format we want to store the texture
-        // 4 & 5th: the final texture width and height(internal_format)
-        // 6th: border: always 0（历史原因）
-        // 7 & 8th: format type and data type of the source image
-        //    We loaded the image with RGB values and stored them as chars (bytes) so we'll pass in the corresponding values
-        // 9th: pointer to the actual image data in memory
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(
-                GL_TEXTURE_2D);//automatically generate all the required mipmaps for the currently bound texture.
-    }
-    else
-        std::cout << "Fail to load texture" << std::endl;
-    // free the memory
-    stbi_image_free(data);
-
-    //-------------------------------------------------------------------------------------------------
-
 
     // build and compile our shader program
     // ------------------------------------
@@ -101,12 +59,32 @@ int main()
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
-            // positions          // colors           // texture coords
+            // positions        // colors          // texture coords
             0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // top right
             0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,   // bottom right
             -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,   // bottom left
             -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f    // top left
     };
+
+//    // Exercise 2:
+//    float vertices[] = {
+//            // positions        // colors          // texture coords
+//            0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  2.0f, 2.0f,   // top right
+//            0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  2.0f, 0.0f,   // bottom right
+//           -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f,   // bottom left
+//           -0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 0.0f,  0.0f, 2.0f    // top left
+//    };
+
+    // Exercise 3: display only the center pixels of the texture image on the rectangle
+    // so that individual pixels are getting visible by changing the texture coordinates.
+//    float vertices[] = {
+//            // positions        // colors          // texture coords
+//            0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  0.55f, 0.55f,   // top right
+//            0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  0.55f, 0.45f,   // bottom right
+//           -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.45f, 0.45f,   // bottom left
+//           -0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 0.0f,  0.45f, 0.55f    // top left
+//    };
+
     unsigned int indices[] = {
             0, 1, 3,
             1, 2, 3
@@ -134,10 +112,104 @@ int main()
     // texture attribute
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) (6 * sizeof(float)));
     glEnableVertexAttribArray(2);
+    //--------------------------------------------------------------
 
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    // glBindVertexArray(0);
+    //-----------------------Texture--------------------------------
+    unsigned int texture1, texture2;
+
+    // refer by ID
+    // glGenTextures():
+    // first is input generate the texture number
+    // second is the array to store the texture ID(now is the single one because is just 1 input)
+    glGenTextures(1, &texture1);
+
+    // Bind the texture
+    glBindTexture(GL_TEXTURE_2D, texture1);
+
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    // first: target of nD textures
+    // second: options we want to set and for which texture axis
+    // third: texture wrapping mode
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    // Exercise 2: display 4 smiley faces on a single container image clamped at its edge
+//    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_CLAMP_TO_EDGE);
+//    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_CLAMP_TO_EDGE);
+
+
+    // 为层级过滤提供放大/缩小的选项，使用 层级or非层级 线性过滤/邻近过滤？
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);//放大用层级过滤不会产生效果，层级过滤只对缩小有效
+
+//    // Exercise 3:
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    //load the picture as texture
+    int width, height, nrChannels;
+
+    // make the y-axis become correct rather than flip down
+    stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
+
+    unsigned char *data = stbi_load("texture/container.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        // first: texture target:generate a texture on the currently bound texture object at the same target
+        // second: mipmap level for which we want to create a texture if you want to set level manually, the base level is 0（层级渐远）
+        // third: what kind of format we want to store the texture
+        // 4 & 5th: the final texture width and height(internal_format)
+        // 6th: border: always 0（历史原因）
+        // 7 & 8th: format type and data type of the source image
+        //    We loaded the image with RGB values and stored them as chars (bytes) so we'll pass in the corresponding values
+        // 9th: pointer to the actual image data in memory
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(
+                GL_TEXTURE_2D);//automatically generate all the required mipmaps for the currently bound texture.
+    }
+    else
+        std::cout << "Fail to load texture" << std::endl;
+    // free the memory
+    stbi_image_free(data);
+
+    // texture2
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,
+                    GL_REPEAT);    // set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // Exercise 3:
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    // load image, create texture and generate mipmaps
+    data = stbi_load("texture/awesomeface.png", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    // tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
+    // -------------------------------------------------------------------------------------------
+    ourShader.use(); // don't forget to activate/use the shader before setting uniforms!
+    // either set it manually like so:
+    glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+    // or set it via the texture class
+    ourShader.setInt("texture2", 1);
+
+    //-------------------------------------------------------------------------------------------------
 
 
     // render loop
@@ -153,8 +225,14 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // bind Texture
-        glBindTexture(GL_TEXTURE_2D, texture);
+        // bind Texture with texture units
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
+        //Exercise 4: set the texture mix value in the shader
+        ourShader.setFloat("mixValue", mixValue);
 
         // render the container
         ourShader.use();
@@ -171,6 +249,7 @@ int main()
     // ------------------------------------------------------------------------
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
@@ -184,6 +263,20 @@ void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    //Exercise 4:
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+    {
+        mixValue += 0.001f; // change this value accordingly (might be too slow or too fast based on system hardware)
+        if (mixValue >= 1.0f)
+            mixValue = 1.0f;
+    }
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+    {
+        mixValue -= 0.001f; // change this value accordingly (might be too slow or too fast based on system hardware)
+        if (mixValue <= 0.0f)
+            mixValue = 0.0f;
+    }
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
--- OpenGL-CLion-MacOS/shader/fS.frag
+++ OpenGL-CLion-MacOS/shader/fS.frag
@@ -5,12 +5,24 @@ in vec3 ourColor;
 in vec2 TexCoord;
 
 // sampler: 采样器
-uniform sampler2D ourTexture;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+// Exercise 4: Use the up and down arrow keys to change how much the container or the smiley face is visible
+uniform float mixValue;
 
 void main()
 {
     // texture(): samples the corresponding color value using the texture parameters we set earlier
     // output is the (filtered) color of the texture at the (interpolated) texture coordinate
     // 翻译：输出就是纹理的（插值）纹理坐标上的(过滤后的)颜色
-    FragColor = texture(ourTexture,TexCoord) * vec4(ourColor, 1.0);// mix the vertices color and texture color
+    //FragColor = texture(ourTexture,TexCoord) * vec4(ourColor, 1.0);// mix the vertices color and texture color
+    // mix():linear interpolate by third param.
+    // FragColor = mix(texture(texture1,TexCoord), texture(texture2,TexCoord),0.2);
+
+    // Exercise 1: change the smile direction
+    //FragColor = mix(texture(texture1,TexCoord), texture(texture2,vec2(1.0-TexCoord.x,TexCoord.y)),0.2);
+
+    // Exercise 4:
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), mixValue);
 }
